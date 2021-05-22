32de834640be986ee734c8558ffb37bd26e9ca9c
Cataholic <965555511@qq.com>
2021-05-22 22:55:17 +0800

    完成阅读量功能

--- blog/models.py
+++ blog/models.py
@@ -62,5 +62,9 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'pk': self.pk})
 
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+
     def __str__(self):
         return self.title
