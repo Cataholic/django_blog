55e6699fa07415817baaa6d385ccba3695f0bc63
Cataholic <965555511@qq.com>
2021-05-29 22:57:10 +0800

    线上部署

--- django_blog/settings/production.py
+++ django_blog/settings/production.py
@@ -1,5 +1,5 @@
 from .common import *
 
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = 'development-secret-key'
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
